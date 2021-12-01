7bed829ab92f8a1cd43f614309bc3c6c77711364
Drake <trinhminh1990@gmail.com>
2021-12-01 12:50:29 +0700

    javascript variables

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
+  let name = 'Vinh'
+  let years = 2021
+  let link = 'https://www.youtube.com/watch?v=Y9gTouaZJ5s&list=PLncHg6Kn2JT4xzJyhXfmJ53dzwVbq-S_E&index=8&ab_channel=H%E1%BB%8FiD%C3%A2nIT'
   return (
     <div className="App">
       <header className="App-header">
-        <h1>day la hello world</h1>
+        <h1>day la hello world with {name} in {years}</h1>
+        <a href={link} target="_blank" >  Click me</a>
       </header>
     </div>
   );
