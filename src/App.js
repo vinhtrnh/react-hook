09c70b1b9f9f2dac9f996776b4aeb1357ab51d16
Drake <trinhminh1990@gmail.com>
2021-12-02 12:50:55 +0700

    event adding

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import Nav from './components/Nav';
 import './App.css';
 
 function App() {
-
   let name = 'Vinh'
-  let years = 2021
-  let link = 'https://www.youtube.com/watch?v=Y9gTouaZJ5s&list=PLncHg6Kn2JT4xzJyhXfmJ53dzwVbq-S_E&index=8&ab_channel=H%E1%BB%8FiD%C3%A2nIT'
+
+  const eventHandle = () => {
+    console.log('Button Clicked')
+  }
+
   return (
     <div className="App">
       <Nav />
       <header className="App-header">
-        <h1>day la hello world with {name} in {years}</h1>
-        <a href={link} target="_blank" >  Click me</a>
+        <h1>day la hello world with {name}</h1>
+        <button type="button" onClick={eventHandle} >click me daddy</button>
       </header>
     </div>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,8 +2,8 @@ import './Nav.scss'
 const Nav = () => {
 
     return (
-        <div class="topnav">
-            <a class="active" href="#home">Home</a>
+        <div className="topnav">
+            <a className="active" href="#home">Home</a>
             <a href="#news">News</a>
             <a href="#contact">Contact</a>
             <a href="#about">About</a>
