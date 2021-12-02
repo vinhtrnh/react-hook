07b5b75ce022d2832a9e25638cfcaa1931cb930c
Drake <trinhminh1990@gmail.com>
2021-12-02 18:21:30 +0700

    function as prop and delete from DOMN

--- src/App.js
+++ src/App.js
@@ -25,11 +25,17 @@ function App() {
   ])
 
   const eventHandle = () => {
+    if (!address) {
+      alert("Please fill the input")
+      return
+    }
+
     let newTodo = {
-      id: 'abc',
+      id: Math.floor((Math.random() * 100) + 1),
       title: address,
       type: 'Drake'
     }
+
     setTodos([...todos, newTodo])
     setAddress('')
   }
@@ -37,14 +43,20 @@ function App() {
     setAddress(event.target.value)
   }
 
+  const deleteTodo = (id) => {
+    let currentTodo = todos
+    currentTodo = currentTodo.filter(item => item.id !== id)
+    setTodos(currentTodo)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <Nav />
         <h1>day la hello world with {name}</h1>
-        <Todos todos={todos} title={'Vinh todos'} />
+        <Todos todos={todos} title={'Vinh todos'} deleteTodo={deleteTodo} />
 
-        <Todos todos={todos.filter(todo => todo.type === 'Drake')} title={'Drake todos'} />
+        <Todos todos={todos.filter(todo => todo.type === 'Drake')} title={'Drake todos'} deleteTodo={deleteTodo} />
 
         <input type="text" value={address} onInput={(event) => { handleEventInput(event) }} />
         <button type="button" onClick={eventHandle} >click me daddy</button>
--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -1,13 +1,20 @@
 const Todos = (props) => {
-    const todos = props.todos
+    const { todos, title, deleteTodo } = props
     // props = property 
     // parent > child, data type top > bottom
+    const handleDeleteEvent = (id) => {
+        deleteTodo(id)
+    }
     return (
         <div className="todo-container">
-            <h3> {props.title} </h3>
+            <h3> {title} </h3>
             {todos.map((todo) => {
                 return (
-                    <li className="todo-child" key={todo.id}> {todo.title} </li>
+                    <div key={todo.id}>
+                        <li className="todo-child" > {todo.title}
+                            &nbsp; &nbsp;     <span onClick={() => handleDeleteEvent(todo.id)}>X</span>
+                        </li>
+                    </div>
                 )
             })}
             <hr />
