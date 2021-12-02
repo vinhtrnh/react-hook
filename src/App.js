77bd699299ac3df3346887e9cdfcb0b5df8ac504
Drake <trinhminh1990@gmail.com>
2021-12-02 14:46:17 +0700

    use state basic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Nav from './components/Nav';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  let name = 'Vinh'
+  const [name, setName] = useState('Eric')
+
+  const [address, setAddress] = useState('')
 
   const eventHandle = () => {
-    console.log('Button Clicked')
+    setName(address)
+  }
+  const handleEventInput = (event) => {
+    setAddress(event.target.value)
   }
 
   return (
@@ -13,6 +19,7 @@ function App() {
       <Nav />
       <header className="App-header">
         <h1>day la hello world with {name}</h1>
+        <input type="text" value={address} onInput={(event) => { handleEventInput(event) }} />
         <button type="button" onClick={eventHandle} >click me daddy</button>
       </header>
     </div>
